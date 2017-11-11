035d013bd2758b1f20bd2df9e5818245666c3249
Heavychanante <jacksonplayer@gmail.com>
2017-11-11 16:30:52 +0100

    Installation of Material Bootstrap for Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+const appRoutes: Routes = [
+    //    { path: 'crisis-center', component: CrisisListComponent },
+    //    { path: 'hero/:id', component: HeroDetailComponent },
+    //    {
+    //        path: 'heroes',
+    //        component: HeroListComponent,
+    //        data: { title: 'Heroes List' }
+    //    },
+    //    {
+    //        path: '',
+    //        redirectTo: '/heroes',
+    //        pathMatch: 'full'
+    //    },
+    //    { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule( {
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MDBBootstrapModule.forRoot(),
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true } // <-- debugging purposes only
+        ),
+
+        BrowserModule
+    ],
+    schemas: [NO_ERRORS_SCHEMA],
+    providers: [],
+    bootstrap: [AppComponent]
+} )
 export class AppModule { }
